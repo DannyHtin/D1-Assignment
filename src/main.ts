31b697a61dc2643a239135dbca96cde1cd9ef9c9
Danny - FinQuantum <dhtin@finquantuminc.com>
2025-10-14 19:35:52 +0000

    Add counter display and increment on button click (Step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
 const clickButton: HTMLButtonElement = document.createElement("button");
 clickButton.textContent = "🚀";
 document.body.append(clickButton);
+
+let counter: number = 0;
+const counterDisplay: HTMLDivElement = document.createElement("div");
+counterDisplay.textContent = `${counter} Thrust`;
+document.body.append(counterDisplay);
+
+clickButton.addEventListener("click", () => {
+  counter++;
+  counterDisplay.textContent = `${counter} Thrust`;
+});
